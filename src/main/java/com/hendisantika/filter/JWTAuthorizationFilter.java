7a096853f1b1775e16f8584c9bc938d88af0cb6a
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-13 05:34:51 +0700

    #44 - Add doFilterInternal in JWTAuthorizationFilter

--- src/main/java/com/hendisantika/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/hendisantika/filter/JWTAuthorizationFilter.java
@@ -1,7 +1,16 @@
 package com.hendisantika.filter;
 
+import com.hendisantika.config.AuthenticationConfigConstants;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +27,20 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(AuthenticationConfigConstants.HEADER_STRING);
+
+        if (header == null || !header.startsWith(AuthenticationConfigConstants.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
 }
