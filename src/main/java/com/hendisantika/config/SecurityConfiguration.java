42e961746dfe17b425ebbc390767463bbbe46203
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-13 05:00:11 +0700

    #37 - Add SecurityConfiguration

---
+++ src/main/java/com/hendisantika/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.hendisantika.config;
+
+import com.hendisantika.service.AuthenticationUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-jwt-authentication-sample1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/13/22
+ * Time: 04:59
+ * To change this template use File | Settings | File Templates.
+ */
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AuthenticationUserDetailService authenticationUserDetailService;
+}
