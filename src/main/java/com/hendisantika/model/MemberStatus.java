218605782ef3b5e1817f6486d6db3c6f73b0e605
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-12 23:18:02 +0700

    #10 - Add MemberStatus Model

---
+++ src/main/java/com/hendisantika/model/MemberStatus.java
@@ -0,0 +1,15 @@
+package com.hendisantika.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-jwt-authentication-sample1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/12/22
+ * Time: 23:17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum MemberStatus {
+    ACTIVE, DEACTIVATED
+}
