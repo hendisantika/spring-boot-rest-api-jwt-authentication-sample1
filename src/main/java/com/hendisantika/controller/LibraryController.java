3146ee2456aa88f2ea3ced0e955def5dadc3c85a
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-13 05:37:50 +0700

    #51 - Add createBook API in LibraryController

--- src/main/java/com/hendisantika/controller/LibraryController.java
+++ src/main/java/com/hendisantika/controller/LibraryController.java
@@ -1,12 +1,15 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.model.Book;
+import com.hendisantika.request.BookCreationRequest;
 import com.hendisantika.service.LibraryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +43,10 @@ public class LibraryController {
     @GetMapping("/book/{bookId}")
     public ResponseEntity<Book> readBook(@PathVariable Long bookId) {
         return ResponseEntity.ok(libraryService.readBook(bookId));
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<Book> createBook(@RequestBody BookCreationRequest request) {
+        return ResponseEntity.ok(libraryService.createBook(request));
     }
 }
