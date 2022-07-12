7ae91f319aaf87334018328353668b79bd9bad80
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-13 05:36:16 +0700

    #47 - Add BCrypt Password bean in Main Class

--- src/main/java/com/hendisantika/SpringBootRestApiJwtAuthenticationSample1Application.java
+++ src/main/java/com/hendisantika/SpringBootRestApiJwtAuthenticationSample1Application.java
@@ -2,6 +2,8 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootRestApiJwtAuthenticationSample1Application {
@@ -10,4 +12,8 @@ public class SpringBootRestApiJwtAuthenticationSample1Application {
         SpringApplication.run(SpringBootRestApiJwtAuthenticationSample1Application.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
