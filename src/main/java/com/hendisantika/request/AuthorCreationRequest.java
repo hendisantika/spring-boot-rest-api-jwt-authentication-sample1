cba2983a456022ca7b9be0cdfd8030bb483146c8
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-12 23:18:43 +0700

    #11 - Add AuthorCreationRequest

---
+++ src/main/java/com/hendisantika/request/AuthorCreationRequest.java
@@ -0,0 +1,19 @@
+package com.hendisantika.request;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-jwt-authentication-sample1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/12/22
+ * Time: 23:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class AuthorCreationRequest {
+    private String firstName;
+    private String lastName;
+}
