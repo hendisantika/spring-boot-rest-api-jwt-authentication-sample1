1fe3ff79093824af5a1a51de21b1c93bce660ac9
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-13 05:00:32 +0700

    #38 - Add configure in SecurityConfiguration

--- src/main/java/com/hendisantika/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/config/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package com.hendisantika.config;
 
 import com.hendisantika.service.AuthenticationUserDetailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -22,4 +25,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthenticationUserDetailService authenticationUserDetailService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, AuthenticationConfigConstants.SIGN_UP_URL).permitAll()
+                //ROLE BASED AUTHENTICATION START
+//            .antMatchers("/api/library/book/**").hasAnyAuthority("USER", "ADMIN")
+//            .antMatchers("/api/library/author/**").hasAnyAuthority("ADMIN")
+//            .antMatchers("/api/library/member/**").hasAnyAuthority("ADMIN")
+                //ROLE BASED AUTHENTICATION END
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 }
