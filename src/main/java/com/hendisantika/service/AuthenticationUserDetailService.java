3b554b1a6712ce0428598d8ac3a21f766ced0c06
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-12 23:24:11 +0700

    #22 - Add loadUserByUsername in AuthenticationUserDetailService

--- src/main/java/com/hendisantika/service/AuthenticationUserDetailService.java
+++ src/main/java/com/hendisantika/service/AuthenticationUserDetailService.java
@@ -1,8 +1,15 @@
 package com.hendisantika.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,18 @@ import org.springframework.stereotype.Service;
 public class AuthenticationUserDetailService implements UserDetailsService {
 
     private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApiUser apiUser = userService.readUserByUsername(username);
+        if (apiUser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(apiUser.getUsername(),
+                apiUser.getPassword(), getAuthorities(apiUser.getRole()));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Arrays.asList(new SimpleGrantedAuthority(role));
+    }
 }
