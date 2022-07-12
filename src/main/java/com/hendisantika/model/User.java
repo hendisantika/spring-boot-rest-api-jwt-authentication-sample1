c2a25844d30d0607badf7312500d5c43d688b12d
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-12 23:13:53 +0700

    #4 - Add User Model

---
+++ src/main/java/com/hendisantika/model/User.java
@@ -0,0 +1,37 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-api-jwt-authentication-sample1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/12/22
+ * Time: 23:12
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
